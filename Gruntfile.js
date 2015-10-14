6561bbbe69122e16a4a0dbe3bf8667fe967790d7
Gerard Ryan <gryan@redhat.com>
2015-10-14 16:11:23 +0100

    Add some initial unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,12 @@ module.exports = function(grunt) {
   'use strict';
 
   grunt.initConfig({
-  //stuff
+    _test_runner: '_mocha',
+    _istanbul: 'istanbul cover --dir',
+    _unit_args: '-A --recursive -t 10000 ./test/unit/',
+
+    unit: '<%= _test_runner %> <%= _unit_args %>',
+    unit_cover: '<%= _istanbul %> cov-unit <%= _test_runner %> -- <%= _unit_args %>'
   });
 
   grunt.loadNpmTasks('grunt-fh-build');
---
+++ test/unit/test_fh_cluster.js
@@ -0,0 +1,98 @@
+var assert = require('assert');
+var _ = require('lodash');
+var os = require('os');
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+
+var cluster = {
+  isMaster: true,
+  isWorker: false,
+  fork: function() {
+    this.isMaster = false;
+    this.isWorker = true;
+  },
+  on: _.noop
+};
+sinon.spy(cluster, 'on');
+sinon.spy(cluster, 'fork');
+
+var fhcluster = proxyquire('../../lib/fh_cluster.js', { cluster: cluster });
+
+describe('fh-cluster', function() {
+
+  describe('With one worker', function() {
+
+    before(function() {
+      cluster.isMaster = true;
+      cluster.isWorker = false;
+      cluster.on.reset();
+      cluster.fork.reset();
+    });
+
+    it('should initially be cluster master', function(done) {
+      assert(cluster.isMaster);
+      done();
+    });
+
+    it('should not call app function as master', function(done) {
+      fhcluster(function() {
+        assert(false, 'App function called by cluster master');
+      }, 1);
+      done();
+    });
+
+    it('should have set a handler for cluster exit event', function(done) {
+      sinon.assert.calledWith(cluster.on, sinon.match('exit'));
+      done();
+    });
+
+    it('should have called cluster.fork', function(done) {
+      sinon.assert.calledOnce(cluster.fork);
+      done();
+    });
+
+    it('should be cluster worker', function(done) {
+      assert(cluster.isWorker);
+      done();
+    });
+
+    it('should call app function when cluster worker', function(done) {
+      fhcluster(function() {
+        assert(cluster.isWorker);
+        done();
+      }, 1);
+    });
+  });
+
+  describe('With two workers', function() {
+
+    before(function() {
+      cluster.isMaster = true;
+      cluster.isWorker = false;
+      cluster.onExitCalled = false; // custom
+      cluster.fork.reset();
+    });
+
+    it('should fork two workers', function(done) {
+      fhcluster(_.noop, 2);
+      sinon.assert.calledTwice(cluster.fork);
+      done();
+    });
+  });
+
+  describe('Not passing optional parameter for number of workers', function() {
+
+    before(function() {
+      cluster.isMaster = true;
+      cluster.isWorker = false;
+      cluster.onExitCalled = false; // custom
+      cluster.fork.reset();
+    });
+
+    it('should fork once for each cpu core', function(done) {
+      fhcluster(_.noop);
+      sinon.assert.callCount(cluster.fork, os.cpus().length);
+      done();
+    });
+  });
+});
